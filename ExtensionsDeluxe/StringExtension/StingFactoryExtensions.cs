0a337bd20ae3e1e8b4be655eb3fc8a4dacd865ef
RobPeterson <robby12345@gmail.com>
2013-05-27 14:58:56 -0400

    Added some preliminary methods.

---
+++ ExtensionsDeluxe/StringExtension/StingFactoryExtensions.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtension
+{
+    public class StingFactoryExtensions
+    {
+        public string ToMorseCode(this string myString)
+        {
+            if (myString == null) return null;
+            StringBuilder sb = new StringBuilder();
+            string morseChar;
+            foreach (char c in myString)
+            {
+                morseChar = TranslateCharToMorseCode(c);
+                if (!String.IsNullOrEmpty(morseChar))
+                {
+                    sb.Append(morseChar);
+                    sb.Append(" ");
+                }
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+        /// <summary>
+        /// Convert a character to a morse code representation.
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        private string TranslateCharToMorseCode(char c)
+        {
+        // http://en.wikipedia.org/wiki/Morse_code#Letters.2C_numbers.2C_punctuation
+            if (c == 'a' || c == 'A') return "·–";
+            if (c == 'b' || c == 'B') return "–···";
+            if (c == 'c' || c == 'C') return "–·–·";
+            if (c == 'd' || c == 'D') return "–··";
+            if (c == 'e' || c == 'E') return "·";
+            if (c == 'f' || c == 'F') return "··–·";
+            if (c == 'g' || c == 'G') return "––·";
+            if (c == 'h' || c == 'H') return "····";
+            if (c == 'i' || c == 'I') return "··";
+            if (c == 'j' || c == 'J') return "·–––";
+            if (c == 'k' || c == 'K') return "–·–";
+            if (c == 'l' || c == 'L') return "·–··";
+            if (c == 'm' || c == 'M') return "––";
+            if (c == 'n' || c == 'N') return "–·";
+            if (c == 'o' || c == 'O') return "–––";
+            if (c == 'p' || c == 'P') return "·––·";
+            if (c == 'q' || c == 'Q') return "––·–";
+            if (c == 'r' || c == 'R') return "·–·";
+            if (c == 's' || c == 'S') return "···";
+            if (c == 't' || c == 'T') return "–";
+            if (c == 'u' || c == 'U') return "··–";
+            if (c == 'v' || c == 'V') return "···–";
+            if (c == 'w' || c == 'W') return "·––";
+            if (c == 'x' || c == 'X') return "–··–";
+            if (c == 'y' || c == 'Y') return "–·––";
+            if (c == 'z' || c == 'Z') return "––··";
+            if (c == '1') return "·––––";
+            if (c == '2') return "··–––";
+            if (c == '3') return "···––";
+            if (c == '4') return "····–";
+            if (c == '5') return "·····";
+            if (c == '6') return "–····";
+            if (c == '7') return "––···";
+            if (c == '8') return "–––··";
+            if (c == '9') return "––––·";
+            if (c == '0') return "–––––";
+            if (c == '.') return "·–·–·–";
+            if (c == ',') return "––··––";
+            if (c == '?') return "··––··";
+            if (c == '\'') return "·––––·";
+            if (c == '!') return "–·–·––";
+            if (c == '/') return "–··–·";
+            if (c == '(') return "–·––·";
+            if (c == ')') return "–·––·–";
+            if (c == '&') return "·–···";
+            if (c == ':') return "–––···";
+            if (c == ';') return "–·–·–·";
+            if (c == '=') return "–···–";
+            if (c == '+') return "·–·–·";
+            if (c == '-') return "–····–";
+            if (c == '_') return "··––·–";
+            if (c == '"') return "·–··–·";
+            if (c == '$') return "···–··–";
+            if (c == '@') return "·––·–·";
+            return "";
+        }
+    }
+}
---
+++ ExtensionsDeluxe/StringExtension/StringModifyingExtensions.cs
@@ -0,0 +1,85 @@
+﻿/* This will provide a collection of extension methods for strings to make life easier for developers.
+ * It make lead go unnecessary code bloat.
+ * Some ideas:
+ *  StringFormatting
+ *  String Analysts, SoundEx, Levenstein,
+ *  Tweet
+ *  FBPost
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtensions
+{
+    public class StringModifyingExtensions
+    {
+        /// <summary>
+        /// Remove any characters that are not digits.
+        /// </summary>
+        /// <param name="myString"></param>
+        public void RemoveNonDigits(this string myString)
+        {
+            if (myString == null) return;
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in myString)
+            {
+                if (Char.IsDigit(c))
+                    sb.Append(c);
+            }
+            myString = sb.ToString();
+        }
+
+        /// <summary>
+        /// Remove any characters that are not letters in the alphabet.
+        /// </summary>
+        /// <param name="myString"></param>
+        public void RemoveNonAlpha(this string myString)
+        {
+            if (myString == null) return;
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in myString)
+            {
+                if (Char.IsLetter(c))
+                    sb.Append(c);
+            }
+            myString = sb.ToString();
+        }
+
+        /// <summary>
+        /// Remove any characters that are punctuation.
+        /// </summary>
+        /// <param name="myString"></param>
+        public void RemovePunctuation(this string myString)
+        {
+            if (myString == null) return;
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in myString)
+            {
+                if (!Char.IsPunctuation(c))
+                    sb.Append(c);
+            }
+            myString = sb.ToString();
+        }
+
+        /// <summary>
+        /// Remove any white space characters.
+        /// </summary>
+        /// <param name="myString"></param>
+        public void RemoveWhiteSpace(this string myString)
+        {
+            if (myString == null) return;
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in myString)
+            {
+                if (!Char.IsWhiteSpace(c))
+                    sb.Append(c);
+            }
+        }
+
+
+    }
+}
