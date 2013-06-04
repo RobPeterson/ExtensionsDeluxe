a489ea839b2ee7f7268fb6d0fa1df56fc11b1e34
Rob Peterson <rpeterson@diplomatpharmacy.com>
2013-06-04 19:04:05 -0400

    Created enum extensions project.

---
+++ ExtensionsDeluxe/EnumExtensions/EnumFactoryExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumExtensions
+{
+    public static class EnumFactoryExtensions
+    {
+        /// <summary>
+        /// Lookup the Description of an Enum value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ToDescription(this Enum value)
+        {
+            if (value == null)
+                return null;
+            var selectedValue = value.GetType().GetField(value.ToString());
+            string description = null;
+
+            if (selectedValue != null)
+            {
+                DescriptionAttribute[] da = (DescriptionAttribute[])(selectedValue.GetCustomAttributes(typeof(DescriptionAttribute), false));
+                description = (da.Length > 0) ? da[0].Description : value.ToString();
+            }
+
+            return description != null ? description : value.ToString();
+        }
+
+    }
+}
