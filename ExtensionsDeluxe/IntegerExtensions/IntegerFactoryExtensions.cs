964a7e8f61c3050b4f7c6cf08ee6d1b06525473b
Robert Peterson <rpeterson@rsilogistics.com>
2015-04-24 17:38:51 -0400

    Added a Factor extension method to int.

--- ExtensionsDeluxe/IntegerExtensions/IntegerFactoryExtensions.cs
+++ ExtensionsDeluxe/IntegerExtensions/IntegerFactoryExtensions.cs
@@ -18,31 +18,107 @@ namespace IntegerExtensions
             //http://stackoverflow.com/questions/7040289/converting-integers-to-roman-numerals
             if (value < 0)
                 throw new ArgumentOutOfRangeException("Please use a positive integer greater than zero.");
-            if (value > 3999 )
+            if (value > 3999)
                 throw new ArgumentOutOfRangeException("Please use a positive integer less than 4000");
 
-                var sb = new StringBuilder();
-                var remain = value;
-                while (remain > 0)
+            var sb = new StringBuilder();
+            var remain = value;
+            while (remain > 0)
+            {
+                if (remain >= 1000)
                 {
-                    if (remain >= 1000) { sb.Append("M"); remain -= 1000; }
-                    else if (remain >= 900) { sb.Append("CM"); remain -= 900; }
-                    else if (remain >= 500) { sb.Append("D"); remain -= 500; }
-                    else if (remain >= 400) { sb.Append("CD"); remain -= 400; }
-                    else if (remain >= 100) { sb.Append("C"); remain -= 100; }
-                    else if (remain >= 90) { sb.Append("XC"); remain -= 90; }
-                    else if (remain >= 50) { sb.Append("L"); remain -= 50; }
-                    else if (remain >= 40) { sb.Append("XL"); remain -= 40; }
-                    else if (remain >= 10) { sb.Append("X"); remain -= 10; }
-                    else if (remain >= 9) { sb.Append("IX"); remain -= 9; }
-                    else if (remain >= 5) { sb.Append("V"); remain -= 5; }
-                    else if (remain >= 4) { sb.Append("IV"); remain -= 4; }
-                    else if (remain >= 1) { sb.Append("I"); remain -= 1; }
-                    else throw new Exception("Unexpected error."); // <<-- shouldn't be possble to get here, but it ensures that we will never have an infinite loop (in case the computer is on crack that day).
+                    sb.Append("M");
+                    remain -= 1000;
                 }
+                else if (remain >= 900)
+                {
+                    sb.Append("CM");
+                    remain -= 900;
+                }
+                else if (remain >= 500)
+                {
+                    sb.Append("D");
+                    remain -= 500;
+                }
+                else if (remain >= 400)
+                {
+                    sb.Append("CD");
+                    remain -= 400;
+                }
+                else if (remain >= 100)
+                {
+                    sb.Append("C");
+                    remain -= 100;
+                }
+                else if (remain >= 90)
+                {
+                    sb.Append("XC");
+                    remain -= 90;
+                }
+                else if (remain >= 50)
+                {
+                    sb.Append("L");
+                    remain -= 50;
+                }
+                else if (remain >= 40)
+                {
+                    sb.Append("XL");
+                    remain -= 40;
+                }
+                else if (remain >= 10)
+                {
+                    sb.Append("X");
+                    remain -= 10;
+                }
+                else if (remain >= 9)
+                {
+                    sb.Append("IX");
+                    remain -= 9;
+                }
+                else if (remain >= 5)
+                {
+                    sb.Append("V");
+                    remain -= 5;
+                }
+                else if (remain >= 4)
+                {
+                    sb.Append("IV");
+                    remain -= 4;
+                }
+                else if (remain >= 1)
+                {
+                    sb.Append("I");
+                    remain -= 1;
+                }
+                else
+                    throw new Exception("Unexpected error.");
+                        // <<-- shouldn't be possble to get here, but it ensures that we will never have an infinite loop (in case the computer is on crack that day).
+            }
 
-                return sb.ToString();
-      
-          }
+            return sb.ToString();
+
+        }
+
+        /// <summary>
+        /// Get a list of factors for an integer. (Slow Method)
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static List<int> Factor(this int number)
+        {
+            var factors = new List<int>();
+            var max = (int) Math.Sqrt(number); //round down
+            for (var factor = 1; factor <= max; ++factor)
+            {
+                //test from 1 to the square root, or the int below it, inclusive.
+                if (number%factor != 0) continue;
+                factors.Add(factor);
+                if (factor != number/factor)
+                {
+                    factors.Add(number/factor);
+                }
+            }
+            return factors;
+        }
     }
 }
