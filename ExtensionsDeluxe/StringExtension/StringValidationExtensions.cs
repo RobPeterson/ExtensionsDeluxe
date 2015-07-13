ddde3ddbc1329158e0b9a18329136511081afb73
Robert Peterson <rpeterson@rsilogistics.com>
2015-07-13 11:01:52 -0400

    Added Mod10Check for Luhns algorithm to check credit card numbers.

--- ExtensionsDeluxe/StringExtension/StringValidationExtensions.cs
+++ ExtensionsDeluxe/StringExtension/StringValidationExtensions.cs
@@ -87,13 +87,43 @@ namespace StringExtension
             DictionaryService.DictServiceSoapClient client = new DictionaryService.DictServiceSoapClient();
             DictionaryService.DictionaryWord[] words;
             words = client.Match(myString,"exact");
-            if (words.Length > 0)
-                result = true;
-            else result = false;
+            result = words.Length > 0;
 
 
             return result;
         }
 
+        /// <summary>
+        /// This will validate the credit card number using the Lunh (Mod 10) algorithm.
+        /// </summary>
+        /// <param name="creditCardNumber"></param>
+        /// <returns></returns>
+        public static bool Mod10Check(this string creditCardNumber)
+        {
+            // http://www.codeproject.com/Tips/515367/Validate-credit-card-number-with-Mod-algorithm
+            //// check whether input string is null or empty
+            if (string.IsNullOrEmpty(creditCardNumber))
+            {
+                return false;
+            }
+
+            /* 1.	Starting with the check digit double the value of every other digit 
+             2.	If doubling of a number results in a two digits number, add up
+               the digits to get a single digit number. This will results in eight single digit numbers                    
+             3. Get the sum of the digits
+             */
+            var sumOfDigits = creditCardNumber.Where((e) => e >= '0' && e <= '9')
+                            .Reverse()
+                            .Select((e, i) => ((int)e - 48) * (i % 2 == 0 ? 1 : 2))
+                            .Sum((e) => e / 10 + e % 10);
+
+
+            //// If the final sum is divisible by 10, then the credit card number
+            //   is valid. If it is not divisible by 10, the number is invalid.            
+            return sumOfDigits % 10 == 0;
+        }
+
     }
+
+
 }
