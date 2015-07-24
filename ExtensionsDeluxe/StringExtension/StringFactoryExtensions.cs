ba6a01a07d11ff9ddb0bb668bbf3300d61f60b74
RobPeterson <peterson123@gmail.com>
2015-07-24 17:19:59 -0400

    Added some test for string extensions and fixed a couple of string extension methods such as Right and Middle.

--- ExtensionsDeluxe/StringExtension/StringFactoryExtensions.cs
+++ ExtensionsDeluxe/StringExtension/StringFactoryExtensions.cs
@@ -31,60 +31,60 @@ namespace StringExtension
         private static string TranslateCharToMorseCode(char c)
         {
         // http://en.wikipedia.org/wiki/Morse_code#Letters.2C_numbers.2C_punctuation
-            if (c == 'a' || c == 'A') return "·–";
-            if (c == 'b' || c == 'B') return "–···";
-            if (c == 'c' || c == 'C') return "–·–·";
-            if (c == 'd' || c == 'D') return "–··";
-            if (c == 'e' || c == 'E') return "·";
-            if (c == 'f' || c == 'F') return "··–·";
-            if (c == 'g' || c == 'G') return "––·";
-            if (c == 'h' || c == 'H') return "····";
-            if (c == 'i' || c == 'I') return "··";
-            if (c == 'j' || c == 'J') return "·–––";
-            if (c == 'k' || c == 'K') return "–·–";
-            if (c == 'l' || c == 'L') return "·–··";
-            if (c == 'm' || c == 'M') return "––";
-            if (c == 'n' || c == 'N') return "–·";
-            if (c == 'o' || c == 'O') return "–––";
-            if (c == 'p' || c == 'P') return "·––·";
-            if (c == 'q' || c == 'Q') return "––·–";
-            if (c == 'r' || c == 'R') return "·–·";
-            if (c == 's' || c == 'S') return "···";
-            if (c == 't' || c == 'T') return "–";
-            if (c == 'u' || c == 'U') return "··–";
-            if (c == 'v' || c == 'V') return "···–";
-            if (c == 'w' || c == 'W') return "·––";
-            if (c == 'x' || c == 'X') return "–··–";
-            if (c == 'y' || c == 'Y') return "–·––";
-            if (c == 'z' || c == 'Z') return "––··";
-            if (c == '1') return "·––––";
-            if (c == '2') return "··–––";
-            if (c == '3') return "···––";
-            if (c == '4') return "····–";
-            if (c == '5') return "·····";
-            if (c == '6') return "–····";
-            if (c == '7') return "––···";
-            if (c == '8') return "–––··";
-            if (c == '9') return "––––·";
-            if (c == '0') return "–––––";
-            if (c == '.') return "·–·–·–";
-            if (c == ',') return "––··––";
-            if (c == '?') return "··––··";
-            if (c == '\'') return "·––––·";
-            if (c == '!') return "–·–·––";
-            if (c == '/') return "–··–·";
-            if (c == '(') return "–·––·";
-            if (c == ')') return "–·––·–";
-            if (c == '&') return "·–···";
-            if (c == ':') return "–––···";
-            if (c == ';') return "–·–·–·";
-            if (c == '=') return "–···–";
-            if (c == '+') return "·–·–·";
-            if (c == '-') return "–····–";
-            if (c == '_') return "··––·–";
-            if (c == '"') return "·–··–·";
-            if (c == '$') return "···–··–";
-            if (c == '@') return "·––·–·";
+            if (c == 'a' || c == 'A') return ".-";
+            if (c == 'b' || c == 'B') return "-...";
+            if (c == 'c' || c == 'C') return "-.-.";
+            if (c == 'd' || c == 'D') return "-..";
+            if (c == 'e' || c == 'E') return ".";
+            if (c == 'f' || c == 'F') return "..-.";
+            if (c == 'g' || c == 'G') return "--.";
+            if (c == 'h' || c == 'H') return "....";
+            if (c == 'i' || c == 'I') return "..";
+            if (c == 'j' || c == 'J') return ".---";
+            if (c == 'k' || c == 'K') return "-.-";
+            if (c == 'l' || c == 'L') return ".-..";
+            if (c == 'm' || c == 'M') return "--";
+            if (c == 'n' || c == 'N') return "-.";
+            if (c == 'o' || c == 'O') return "---";
+            if (c == 'p' || c == 'P') return ".--.";
+            if (c == 'q' || c == 'Q') return "--.-";
+            if (c == 'r' || c == 'R') return ".-.";
+            if (c == 's' || c == 'S') return "...";
+            if (c == 't' || c == 'T') return "-";
+            if (c == 'u' || c == 'U') return "..-";
+            if (c == 'v' || c == 'V') return "...-";
+            if (c == 'w' || c == 'W') return ".--";
+            if (c == 'x' || c == 'X') return "-..-";
+            if (c == 'y' || c == 'Y') return "-.--";
+            if (c == 'z' || c == 'Z') return "--..";
+            if (c == '1') return ".----";
+            if (c == '2') return "..---";
+            if (c == '3') return "...--";
+            if (c == '4') return "....-";
+            if (c == '5') return ".....";
+            if (c == '6') return "-....";
+            if (c == '7') return "--...";
+            if (c == '8') return "---..";
+            if (c == '9') return "----.";
+            if (c == '0') return "-----";
+            if (c == '.') return ".-.-.-";
+            if (c == ',') return "--..--";
+            if (c == '?') return "..-..";
+            if (c == '\'') return ".----.";
+            if (c == '!') return "-.-.--";
+            if (c == '/') return "-..-.";
+            if (c == '(') return "-.--.";
+            if (c == ')') return "-.--.-";
+            if (c == '&') return ".-...";
+            if (c == ':') return "---...";
+            if (c == ';') return "-.-.-.";
+            if (c == '=') return "-...-";
+            if (c == '+') return ".-.-.";
+            if (c == '-') return "-....-";
+            if (c == '_') return "..--.-";
+            if (c == '"') return ".-..-.";
+            if (c == '$') return "...-..-";
+            if (c == '@') return ".--.-.";
             return "";
         }
 
@@ -123,7 +123,7 @@ namespace StringExtension
             {
                 return myString;
             }
-            startIndex = myString.Length - numberOfCharacters - 1;
+            startIndex = myString.Length - numberOfCharacters;
             result = myString.Substring(startIndex, numberOfCharacters);
             return result;
         }
@@ -143,7 +143,7 @@ namespace StringExtension
             if (myString.Length == 0) return "";
             if (myString.Length <= endIndex)
                 endIndex = myString.Length - 1;
-            netLength = startIndex - endIndex;
+            netLength = endIndex - startIndex + 1;
             result = myString.Substring(startIndex, netLength);
             return result;
         }
@@ -206,7 +206,7 @@ namespace StringExtension
             {
                 anEnumValue = GetEnumValueFromDescription<T>(description);
             }
-            catch (Exception ex)
+            catch (Exception)
             {
                 result = false;
             }
---
+++ ExtensionsDeluxe/UnitTestProject/UnitTestStringExtensions.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringExtension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTestStringExtensions
+    {
+        [TestMethod]
+        public void TestToMorseCode()
+        {
+            const string test = "sos";
+            const string expected = "... --- ...";
+            var result = test.ToMorseCode();
+            Assert.IsTrue(result.Equals(expected));
+        }
+
+        [TestMethod]
+        public void TestToMorseCodeT()
+        {
+            const string test = "T";
+            var result = test.ToMorseCode();
+            Assert.IsTrue(result.CompareTo("-") == 0);
+        }
+
+        [TestMethod]
+        public void TestToMorseCodeE()
+        {
+            const string test = "E";
+            var result = test.ToMorseCode();
+            Assert.IsTrue(result.CompareTo(".") == 0);
+        }
+
+        [TestMethod]
+        public void TestLeft()
+        {
+            const string test = "test";
+            var result = test.Left(2);
+            Assert.IsTrue(result.Equals("te"));
+        }
+
+        [TestMethod]
+        public void TestLeftExceedsCount()
+        {
+            const string test = "test";
+            var result = test.Left(5);
+            Assert.IsTrue(result.Equals("test"));
+        }
+
+        [TestMethod]
+        public void TestLeftNull()
+        {
+            string test = null;
+            var result = test.Left(5);
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void TestRight()
+        {
+            const string test = "test";
+            var result = test.Right(2);
+            Assert.IsTrue(result.Equals("st"));
+        }
+
+        [TestMethod]
+        public void TestRightExceedsCount()
+        {
+            const string test = "test";
+            var result = test.Right(5);
+            Assert.IsTrue(result.Equals("test"));
+        }
+
+        [TestMethod]
+        public void TestRightNull()
+        {
+            string test = null;
+            var result = test.Right(5);
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void TestMiddle()
+        {
+            const string test = "test";
+            var result = test.Middle(1,2);
+            Assert.IsTrue(result.Equals("es"));
+        }
+
+
+        [TestMethod]
+        public void TestMiddleNull()
+        {
+            string test = null;
+            var result = test.Middle(1,2);
+            Assert.IsNull(result);
+        }
+    }
+}
